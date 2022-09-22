8c654028b68bc78710f5f5092bf9585b6f479171
hvictor186 <hvictorleite@gmail.com>
2022-09-22 14:59:02 -0300

    Entity Framework packages installed and configured to connect to database.

--- LanchesMac/Program.cs
+++ LanchesMac/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using LanchesMac.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
