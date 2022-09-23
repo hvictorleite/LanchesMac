7167c828e377d37a2ee09f8316772a6dff91541e
hvictor186 <hvictorleite@gmail.com>
2022-09-23 13:47:23 -0300

    Implemented 'Repository' classes and interfaces. Configured as dependency injection.

--- LanchesMac/Program.cs
+++ LanchesMac/Program.cs
@@ -1,12 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 using LanchesMac.Context;
+using LanchesMac.Repositories.Interfaces;
+using LanchesMac.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<ILancheRepository, LancheRepository>();
+builder.Services.AddTransient<ICategoriaRepository, CategoriaRepository>();
 
 var app = builder.Build();
 
