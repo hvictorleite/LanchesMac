61ba8a984ad5d7845f351779020a4271e34f0adb
hvictor186 <hvictorleite@gmail.com>
2022-09-23 16:16:13 -0300

    Created model 'CarrinhoCompraItem', added to 'AppDbContext' and created in database as table using Migration.

--- LanchesMac/Context/AppDbContext.cs
+++ LanchesMac/Context/AppDbContext.cs
@@ -7,6 +7,7 @@ namespace LanchesMac.Context
     {
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Lanche> Lanches { get; set; }
+        public DbSet<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
         
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
