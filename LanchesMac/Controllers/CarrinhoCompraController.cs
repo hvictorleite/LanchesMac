f4da9d776c9ddf9a2a05bc9f43697378138d3f43
Victor Hugo Marques Rosa Leite <hvictorleite@gmail.com>
2022-09-26 14:27:16 -0300

    Created 'CarrinhoCompraController' and 'Index', 'AdicionarItemNoCarrinhoCompra' and 'RemoverItemDoCarrinhoCompra' actions.

---
+++ LanchesMac/Controllers/CarrinhoCompraController.cs
@@ -0,0 +1,53 @@
+﻿using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class CarrinhoCompraController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraController(ILancheRepository lancheRepository, CarrinhoCompra carrinhoCompra)
+        {
+            _lancheRepository = lancheRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IActionResult Index()
+        {
+            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.CarrinhoCompraItens = itens;
+
+            var carrinhoCompraViewModel = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+
+            return View(carrinhoCompraViewModel);
+        }
+
+        public RedirectToActionResult AdicionarItemNoCarrinhoCompra(int lancheId)
+        {
+            var lanche = _lancheRepository.GetLancheById(lancheId);
+            
+            if (lanche != null)
+                _carrinhoCompra.AdicionarAoCarrinho(lanche);
+
+            return RedirectToAction("Index");
+        }
+
+        public RedirectToActionResult RemoverItemDoCarrinhoCompra(int lancheId)
+        {
+            var lanche = _lancheRepository.GetLancheById(lancheId);
+
+            if (lanche != null)
+                _carrinhoCompra.RemoverDoCarrinho(lanche);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
