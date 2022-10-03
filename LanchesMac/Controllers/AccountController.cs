79bfd0b09cc7d708f1e99e7c61b3b3f767de7a10
hvictor186 <hvictorleite@gmail.com>
2022-10-03 13:47:32 -0300

    Created '_LoginPartial' with routes to access 'Login', 'Register' and 'Logout' views/actions.

--- LanchesMac/Controllers/AccountController.cs
+++ LanchesMac/Controllers/AccountController.cs
@@ -69,5 +69,14 @@ namespace LanchesMac.Controllers
             }
             return View(registroVM);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            HttpContext.Session.Clear();
+            HttpContext.User = null;
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
