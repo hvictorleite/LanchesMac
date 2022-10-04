b639a66be6d69eebc14c96f645950c5d8179451b
hvictor186 <hvictorleite@gmail.com>
2022-10-04 14:49:12 -0300

    Installed 'ReflectionIt.Mvc.Paging' package to apply pagination in views.

--- LanchesMac/Program.cs
+++ LanchesMac/Program.cs
@@ -5,6 +5,7 @@ using LanchesMac.Repositories;
 using LanchesMac.Models;
 using Microsoft.AspNetCore.Identity;
 using LanchesMac.Services;
+using ReflectionIT.Mvc.Paging;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,6 +29,11 @@ builder.Services.AddScoped(sp => CarrinhoCompra.GetCarrinho(sp));
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddPaging(options =>
+{
+    options.ViewName = "Bootstrap4";
+    options.PageParameterName = "pageindex";
+});
 
 builder.Services.AddMemoryCache();
 builder.Services.AddSession();
