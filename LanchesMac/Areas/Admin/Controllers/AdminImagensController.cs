6ed4e342a59a3501a126dd21184e71127fa30a8d
hvictor186 <hvictorleite@gmail.com>
2022-10-05 15:52:10 -0300

    Created and configured 'ConfigurationImagens', created 'AdminImagens' controller and view and  added nav-link in '_Layout'.

---
+++ LanchesMac/Areas/Admin/Controllers/AdminImagensController.cs
@@ -0,0 +1,27 @@
+﻿using LanchesMac.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace LanchesMac.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+
+    public class AdminImagensController : Controller
+    {
+        private readonly ConfigurationImagens _myConfig;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public AdminImagensController(IWebHostEnvironment hostingEnvironment, IOptions<ConfigurationImagens> myConfig)
+        {
+            _myConfig = myConfig.Value;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
