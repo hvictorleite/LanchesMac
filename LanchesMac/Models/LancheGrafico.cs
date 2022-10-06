7976393b50c451fb9deb9b7abe67a91c1c3bc4bb
hvictor186 <hvictorleite@gmail.com>
2022-10-06 14:10:41 -0300

    Modeled 'LanchesGrafico' to display charts using GoogleChart.

---
+++ LanchesMac/Models/LancheGrafico.cs
@@ -0,0 +1,9 @@
+﻿namespace LanchesMac.Models
+{
+    public class LancheGrafico
+    {
+        public string NomeLanche { get; set; }
+        public int LanchesQuantidade { get; set; }
+        public decimal LanchesValorTotal { get; set; }
+    }
+}
