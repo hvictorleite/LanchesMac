bfb1bbfa2082f1ef62902bd98b02e4b6a76f73fc
hvictor186 <hvictorleite@gmail.com>
2022-10-06 13:44:05 -0300

    Inplemented get and delete actions for images.

--- LanchesMac/Areas/Admin/Controllers/AdminImagensController.cs
+++ LanchesMac/Areas/Admin/Controllers/AdminImagensController.cs
@@ -1,6 +1,8 @@
-﻿using LanchesMac.Models;
+﻿using AspNetCore;
+using LanchesMac.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Operations;
 using Microsoft.Extensions.Options;
 
 namespace LanchesMac.Areas.Admin.Controllers
@@ -65,6 +67,36 @@ namespace LanchesMac.Areas.Admin.Controllers
             ViewBag.Arquivos = filePathsName;
 
             return View(ViewData);
+        }
+
+        public IActionResult GetImagens()
+        {
+            FileManagerModel model = new FileManagerModel();
+            var userImagesPath = Path.Combine(_hostingEnvironment.WebRootPath,
+                                              _myConfig.NomePastaImagensProdutos);
+
+            DirectoryInfo dir = new DirectoryInfo(userImagesPath);
+            FileInfo[] files = dir.GetFiles();
+            model.PathImagesProduto = _myConfig.NomePastaImagensProdutos;
+
+            if (files.Length == 0)
+                ViewData["Erro"] = $"Nenhum arquivo encontrado na pasta {userImagesPath}";
+
+            model.Files = files;
+            return View(model);
+        }
+
+        public IActionResult DeleteFile(string fname)
+        {
+            string _imageDeleta = Path.Combine(_hostingEnvironment.WebRootPath, _myConfig.NomePastaImagensProdutos + "\\", fname );
+
+            if (System.IO.File.Exists(_imageDeleta))
+            {
+                System.IO.File.Delete(_imageDeleta);
+                ViewData["Deletado"] = $"Arquivo(s) {_imageDeleta} deletado(s) com sucesso";
+            }
+
+            return View("Index");
         }
     }
 }
