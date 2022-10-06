43adb7948e22a268b1045b4fc5d74fdfb9015cb5
hvictor186 <hvictorleite@gmail.com>
2022-10-06 14:14:37 -0300

    Created method to get a list of 'LancheGrafico' that were sold within n days.

---
+++ LanchesMac/Areas/Admin/Servicos/GraficoVendasService.cs
@@ -0,0 +1,47 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+
+namespace LanchesMac.Areas.Admin.Servicos
+{
+    public class GraficoVendasService
+    {
+        private readonly AppDbContext _context;
+
+        public GraficoVendasService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<LancheGrafico> GetVendasLanche(int dias = 360)
+        {
+            var data = DateTime.Now.AddDays(-dias);
+
+            var lanches = (from pd in _context.PedidoDetalhes
+                           join l in _context.Lanches on pd.LancheId equals l.LancheId
+                           where pd.Pedido.PedidoEnviado >= data
+                           group pd by new { pd.LancheId, l.Nome, pd.Quantidade }
+                           into g
+                           select new
+                           {
+                               NomeLanche = g.Key.Nome,
+                               LanchesQuantidade = g.Sum(q => q.Quantidade),
+                               LanchesValorTotal = g.Sum(a => a.Preco * a.Quantidade)
+                           });
+
+            var lista = new List<LancheGrafico>();
+
+            foreach (var item in lanches)
+            {
+                var lanche = new LancheGrafico
+                {
+                    NomeLanche = item.NomeLanche,
+                    LanchesQuantidade = item.LanchesQuantidade,
+                    LanchesValorTotal= item.LanchesValorTotal
+                };
+                lista.Add(lanche);
+            }
+
+            return lista;
+        }
+    }
+}
